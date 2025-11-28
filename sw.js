f5b5ae3ee5b54397efba235d7551d6c0562ee0db
massive target <massivetarget@gmail.com>
2025-11-29 04:27:51 +0530

    first commit

---
+++ sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = "journal-pwa-cache-v1";
+
+const URLS_TO_CACHE = ["/6pwaServiceWorker.html", "/all.min.css"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+
+
+// const CACHE_NAME = "journal-pwa-cache-v1";
+
+// const URLS_TO_CACHE = ["6pwaServiceWorker.html", "/all.min.css"];
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+
+
+
+// const CACHE_NAME = "journal-pwa-cache-v1";
+// const URLS_TO_CACHE = ["/5pwa.html", "/all.min.css"];
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(
+//       (response) => response || fetch(event.request)
+//     )
+//   );
+// });
